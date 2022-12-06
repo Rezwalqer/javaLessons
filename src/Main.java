d1911fe9d89a23271b0b61ec18a5b597210f93e7
Ruslan <rezwalqer@gmail.com>
2022-12-06 13:42:20 +0500

    Домашнее задание от 05.12.22 Исправление от 06,12,22

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,6 @@ public class Main {
         task17();
         task18();
     }
-
     public static void task1() {
         System.out.println("Задача для самостоятельной работы 1");
         for (int i = 1; i < 11; i = i + 1) {
@@ -27,7 +26,6 @@ public class Main {
         }
         System.out.println();
     }
-
     public static void task2() {
         System.out.println("Задача для самостоятельной работы 2");
         for (int i = 10; i > 0; i = i - 1) {
@@ -35,7 +33,6 @@ public class Main {
         }
         System.out.println();
     }
-
     public static void task3() {
         System.out.println("Задача для самостоятельной работы 3");
         for (int i = 0; i < 17; i = i + 2) {
@@ -43,7 +40,6 @@ public class Main {
         }
         System.out.println();
     }
-
     public static void task4() {
         System.out.println("Задача для самостоятельной работы 4");
         for (int i = 10; i > -11; i = i - 1) {
@@ -51,14 +47,15 @@ public class Main {
         }
         System.out.println();
     }
-
     public static void task5() {
         System.out.println("Задача для самостоятельной работы 5");
         for (int i = 1904; i < 2097; i = i + 4) {
+            if (i % 100 == 0 && i % 400 != 0){
+                continue;
+            }
             System.out.println(i + " год является високосным");
         }
     }
-
     public static void task6() {
         System.out.println("Задача для самостоятельной работы 6");
         for (int i = 7; i < 99; i += 7) {
@@ -66,16 +63,13 @@ public class Main {
         }
         System.out.println();
     }
-
     public static void task7() {
         System.out.println("Задача для самостоятельной работы 7");
         for (int i = 1; i < 513; i *= 2) {
             System.out.print(i + " ");
         }
         System.out.println();
-
     }
-
     public static void task8() {
         System.out.println("Задача для самостоятельной работы 8");
         int total = 29000;
@@ -86,10 +80,12 @@ public class Main {
 
     public static void task9() {
         System.out.println("Задача для самостоятельной работы 9");
-        float total = 29000f;
-        for (int i = 0; i < 13; i++) {
-            total *= 1.01;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + total);
+        double total = 29000f;
+        double percent = 1.01;
+        double capital = 29000f;
+        for (int i = 1; i < 13; i++) {
+            capital = total * percent + capital;
+            System.out.println("Спустя " + i + " месяц(ев) сумма накоплений равна " + (int) capital);
         }
     }
 
@@ -106,16 +102,17 @@ public class Main {
         }
         System.out.println();
     }
-
     public static void task11() {
         System.out.println("Задача для самостоятельной работы 11");
         int amount = 15000;
         int month = 0;
-        while (amount < 2_459_000) {
-            amount += 15000 + amount * 0.01;
+        double percent = 1.01;
+        double total = 15000;
+        while (total < 2_459_000) {
+            total = amount + total * percent;
             month += 1;
+            System.out.println("Спустя " + month + " месяц(ев), сумма накоплений равна " + (int) total + " рублей.");
         }
-        System.out.println("Месяц " + month + ", сумма накоплений равна " + amount + " рублей. Чтобы накопить 2459000 рублей потребуется " + (month + 1) + " месяцев");
     }
 
     public static void task12() {
@@ -173,10 +170,10 @@ public class Main {
         float total = 15_000f;
         int monthCounter = 0;
         float percent = 1.07f;
-        while (monthCounter<=(12 * 9)){
+        while (monthCounter <= (12 * 9)) {
             total *= percent;
             monthCounter += 1;
-            if (monthCounter % 6 == 0){
+            if (monthCounter % 6 == 0) {
                 System.out.println("Месяц: " + monthCounter + ". Накопления: " + (int) total);
             }
         }
@@ -187,7 +184,7 @@ public class Main {
         System.out.println("Задача для самостоятельной работы 17");
         int firstFriday = 4;
         int daysInMonth = 31;
-        for (; firstFriday<= daysInMonth; firstFriday += 7){
+        for (; firstFriday <= daysInMonth; firstFriday += 7) {
             System.out.println("Сегодня пятница, " + firstFriday + "-е число. Необходимо подготовить отчет.");
         }
     }
@@ -197,13 +194,11 @@ public class Main {
         int thisYear = 2022;
         int previousYears = thisYear - 200;
         int nextYears = thisYear + 100;
-        for (int i = previousYears; i <= nextYears; i++){
-            if (i % 79 == 0){
+        for (int i = previousYears; i <= nextYears; i++) {
+            if (i % 79 == 0) {
                 System.out.println(i);
 
             }
         }
     }
-
-
 }
