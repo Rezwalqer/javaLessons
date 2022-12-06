e368566bacf17e3083ad6b66d858886238eb755b
Ruslan <rezwalqer@gmail.com>
2022-12-06 13:45:03 +0500

    Домашнее задание от 05.12.22 Исправление от 06,12,22

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,7 @@ public class Main {
         task17();
         task18();
     }
+
     public static void task1() {
         System.out.println("Задача для самостоятельной работы 1");
         for (int i = 1; i < 11; i = i + 1) {
@@ -26,6 +27,7 @@ public class Main {
         }
         System.out.println();
     }
+
     public static void task2() {
         System.out.println("Задача для самостоятельной работы 2");
         for (int i = 10; i > 0; i = i - 1) {
@@ -33,6 +35,7 @@ public class Main {
         }
         System.out.println();
     }
+
     public static void task3() {
         System.out.println("Задача для самостоятельной работы 3");
         for (int i = 0; i < 17; i = i + 2) {
@@ -40,6 +43,7 @@ public class Main {
         }
         System.out.println();
     }
+
     public static void task4() {
         System.out.println("Задача для самостоятельной работы 4");
         for (int i = 10; i > -11; i = i - 1) {
@@ -47,15 +51,17 @@ public class Main {
         }
         System.out.println();
     }
+
     public static void task5() {
         System.out.println("Задача для самостоятельной работы 5");
         for (int i = 1904; i < 2097; i = i + 4) {
-            if (i % 100 == 0 && i % 400 != 0){
+            if (i % 100 == 0 && i % 400 != 0) {
                 continue;
             }
             System.out.println(i + " год является високосным");
         }
     }
+
     public static void task6() {
         System.out.println("Задача для самостоятельной работы 6");
         for (int i = 7; i < 99; i += 7) {
@@ -63,6 +69,7 @@ public class Main {
         }
         System.out.println();
     }
+
     public static void task7() {
         System.out.println("Задача для самостоятельной работы 7");
         for (int i = 1; i < 513; i *= 2) {
@@ -70,6 +77,7 @@ public class Main {
         }
         System.out.println();
     }
+
     public static void task8() {
         System.out.println("Задача для самостоятельной работы 8");
         int total = 29000;
@@ -102,6 +110,7 @@ public class Main {
         }
         System.out.println();
     }
+
     public static void task11() {
         System.out.println("Задача для самостоятельной работы 11");
         int amount = 15000;
